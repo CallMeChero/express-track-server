1bf0b2252245695facaa1f54f0d43352db0559a9
CallMeChero <david.ceranic@gmail.com>
2020-12-23 11:06:28 +0100

    Initial commit with configured MongoDB, nodemon  & first POST request

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple ExpressAPI project with MongoDB for making use of API calls #
+*** npm install bcrypt express jsonwebtoken mongoose nodemon ***
+## MongoDB - cloud.mongodb.com ##
+
+## Nodemon ##
+> watches for changes in our project
---
+++ src/index.js
@@ -0,0 +1,32 @@
+require('./models/User');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+// required to parse JSON that comes from FE
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const connectionString = 'mongodb+srv://dceranic:dceranic@react-native-learning.2wfqr.mongodb.net/<dbname>?retryWrites=true&w=majority';
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to mongo', err);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hi there!');
+});
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+});
